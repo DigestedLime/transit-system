70e57d6371f05e4816eb7efe4ade5fbb2e76d043
207f20 <70546066+207f20@users.noreply.github.com>
2020-10-26 12:41:18 -0400

    Initial commit

---
+++ src/transitapp/Main.java
@@ -0,0 +1,9 @@
+// These is a placeholder package and placeholder class
+// Feel free to rename or remove these when you add in your own code (just make sure to add/commit/push any changes made,
+//		and let your teammates know to pull the changes. Follow the workflow in the a2 instructions)
+
+package transitapp;
+
+public class Main {
+
+}
