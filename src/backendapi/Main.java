ecbb1c7dee622005c7c9f34483b2950b32879c65
Aaron Hong <aaronr.hong@mail.utoronto.ca>
2020-11-13 16:00:06 -0500

    Simplified Route Parsing
    Route maps are now read under a single file.

--- src/backendapi/Main.java
+++ src/backendapi/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
 	public static void main(String args[]) {
 		RouteMap subway = new RouteMap();
-		subway.initialize("subway_map.txt", "bus_map.txt");
+		subway.initialize("subway_map.txt");
 		System.out.println(subway.getRouteMap());
 	}
-	
+
 }
