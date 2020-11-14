eab4e82376aefb9f609bedc166da9dd2c5e5142f
rehmat10 <61368086+rehmat10@users.noreply.github.com>
2020-11-14 11:24:21 -0500

    Added exit_station method to Trip.java, reimplemented how cost is determined

--- src/routenetwork/Trip.java
+++ src/routenetwork/Trip.java
@@ -19,8 +19,40 @@ public class Trip {
 		this.route = new ArrayList<Station>();
 		this.current = start;
 		this.route.add(start);
+		if (this.current.getFareType() == "BUS") {
+			this.total_cost = 2;
+		}
 	}
 	
+	public void enter_station(Station station) {
+		if (this.current.getFareType() == "TRAIN") {
+			this.total_cost += 0.5;
+		}
+		this.current = station;
+		this.route.add(this.current);
+		if (this.current.getFareType() == "BUS") {
+			this.total_cost += 2;
+		}
+		
+		if (this.total_cost > 6) {
+			this.total_cost = 6;
+		}
+	}
+	/**
+	 * Sets current station to null thus ending the trip
+	 * 
+	 * @return true if it made sense (i.e. they were in a station to exit)
+	 *         and false otherwise (i.e. there were not in a station before leaving)
+	 */
+	public boolean exit_station() {
+		if (this.current == null) {
+			return false;
+		}
+		this.current = null;
+		return true;
+	}
+	
+	/***
 	// Currently simply caps cost at 6, need to consider time eventually
 	public void addStop(Station stop) {
 		this.route.add(stop);
@@ -29,8 +61,6 @@ public class Trip {
 		} else if (this.current.getFareType() == "TRAIN") {
 			this.total_cost += 0.5;
 		}
-		if (this.total_cost > 6) {
-			this.total_cost = 6;
-		}
-	}
+		
+	}***/
 }
