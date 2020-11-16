2cf1ac2228505e90544dacbaca75e7697d4e675d
Akira <akira.takaki@mail.utoronto.ca>
2020-11-16 16:57:56 -0500

    Fixed pothole_case.

--- src/routenetwork/Trip.java
+++ src/routenetwork/Trip.java
@@ -10,41 +10,41 @@ import java.util.ArrayList;
  */
 public class Trip {
 	
-	private float total_cost;
+	private float totalCost;
 	private ArrayList<Station> route;
 	private Station current;
 	private int twoHourPeriods;
-	private int total_minutes;
+	private int totalMinutes;
 	
 	public Trip(Station start) {
-		this.total_cost = 0;
+		this.totalCost = 0;
 		this.route = new ArrayList<Station>();
 		this.current = start;
-		this.total_minutes = 0;
+		this.totalMinutes = 0;
 		this.twoHourPeriods = 1;
 		
 		this.route.add(start);
 		if (this.current.getFareType() == "BUS") {
-			this.total_cost = 2;
+			this.totalCost = 2;
 		}
 	}
 	
 	public void enter_station(Station station, int minutes) {
 		if (this.current.getFareType() == "TRAIN") {
-			this.total_cost += 0.5;
+			this.totalCost += 0.5;
 		}
 		this.current = station;
 		this.route.add(this.current);
 		if (this.current.getFareType() == "BUS") {
-			this.total_cost += 2;
+			this.totalCost += 2;
 		}
 		
-		if (this.total_cost > 6 * this.twoHourPeriods && this.total_minutes < 120 * this.twoHourPeriods) {
-			this.total_cost = 6 * this.twoHourPeriods;
+		if (this.totalCost > 6 * this.twoHourPeriods && this.totalMinutes < 120 * this.twoHourPeriods) {
+			this.totalCost = 6 * this.twoHourPeriods;
 		}
 		
-		this.total_minutes += minutes;
-		this.twoHourPeriods = (this.total_minutes / 120) + 1;
+		this.totalMinutes += minutes;
+		this.twoHourPeriods = (this.totalMinutes / 120) + 1;
 	}
 	/**
 	 * Sets current station to null thus ending the trip
@@ -52,7 +52,7 @@ public class Trip {
 	 * @return true if it made sense (i.e. they were in a station to exit)
 	 *         and false otherwise (i.e. there were not in a station before leaving)
 	 */
-	public boolean exit_station() {
+	public boolean exitStation() {
 		if (this.current == null) {
 			return false;
 		}
@@ -64,8 +64,8 @@ public class Trip {
 	 * 
 	 * @return The cost of this trip
 	 */
-	public float get_cost() {
-		return this.total_cost;
+	public float getCost() {
+		return this.totalCost;
 	}
 	
 	/***
