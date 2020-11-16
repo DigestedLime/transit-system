0e48c52ffbb93d0796636631ca0f0385eb2c8092
KennyChiWa-Wa <67726409+KennyChiWa-Wa@users.noreply.github.com>
2020-11-15 23:24:44 -0500

    repopulated travel

--- src/transitapp/FXMLTravelController.java
+++ src/transitapp/FXMLTravelController.java
@@ -2,6 +2,8 @@ package transitapp;
 
 import java.io.IOException;
 
+import backendapi.RouteMap;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -9,7 +11,14 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import routenetwork.RouteController;
 
+/**
+ * This class is responsible for the user travelling with card information, and giving a status for the user
+ * to know if the trip was successful.
+ * @author 
+ *
+ */
 public class FXMLTravelController extends ControllerParent implements Initializable {
 	
 	@FXML
@@ -24,9 +33,29 @@ public class FXMLTravelController extends ControllerParent implements Initializa
 	public ImageView error;
 	@FXML
 	public ImageView checkmark;
+	@FXML
+    public ListView<String> departingStation;
+    @FXML
+    public ListView<String> terminusStation;
+    public RouteController routeController;
 	
 	
 	public void startJourneyPush(ActionEvent event) throws IOException {
 		// TODO
 	}
+	
+	/**
+     * Method that needs to be in the class from implementing Initializable.
+     */
+    @SuppressWarnings("unchecked")
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+    	
+        RouteMap temp = new RouteMap();
+        temp.initialize("subway_map.txt");
+        this.routeController = temp.getRouteMap();
+        
+        departingStation.setItems(FXCollections.observableArrayList(this.routeController.getAllStations()));
+        terminusStation.setItems(FXCollections.observableArrayList(this.routeController.getAllStations()));
+    }
 }
