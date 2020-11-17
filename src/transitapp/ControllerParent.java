e8f2bb7f5bcee28f70c81434f2eb56a8d94c8159
KennyChiWa-Wa <67726409+KennyChiWa-Wa@users.noreply.github.com>
2020-11-16 23:57:36 -0500

    Added citations for the ControllerParent class that would apply to all of the children controller classes as well.

--- src/transitapp/ControllerParent.java
+++ src/transitapp/ControllerParent.java
@@ -18,7 +18,29 @@ import javafx.stage.Stage;
  * This parent class is responsible for making controller children and
  * reducing the amount of code that is written. This class has the exit button
  * and the general layout for changing scenes when a button is pressed.
- * @author Lap Khang Tran
+ * 
+ * Citing (note: these were all used in the controller classes to implement the different screens that were used and on how to use the screens using javafx)
+ * 
+ * 		- https://www.youtube.com/watch?v=XCgcQTQCfJQ&t=118s&ab_channel=JaretWright
+ * 			- this was mainly used as a guide on how the controllers in the system works, and how we would move on to different
+ * 			  scenes using the controllers. Also used the video as a tutorial on how to use scenebuilder and how FXML files
+ * 			  work, and how to hook up methods and objects to the objects that are made in the scenes when using scenebuilder.
+ * 			- this code from the video was used in ALL the controller classes.
+ * 
+ * 		- https://www.youtube.com/watch?v=L72FEmEt-G8
+ * 			- this was used to make the window of the applcation borderless
+ * 
+ * 		- https://www.youtube.com/watch?v=cwJK_WpseKQ&t=138s
+ * 			- this was used to make take the information from the javafx textboxes so that we can store the information
+ * 
+ * 		- https://www.youtube.com/watch?v=ULuXfAm3HLg&t=903s&ab_channel=JavaCodes
+ * 			- this was used to see how textfields in javafx would work
+ * 
+ * 		- https://www.youtube.com/watch?v=Z1W4E2d4Yxo&ab_channel=GenuineCoder
+ * 			- this was used when first using scenebuilder to make the scenes for the GUI
+ * 		
+ * 		- 
+ * @author
  *
  */
 
